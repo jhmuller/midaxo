2cb414ca9b6e23103a960f1153afe78d6f6027be
John H. Muller <jmuller.ics88@gtalumni.org>
2018-08-02 12:39:08 -0400

    now with 26 company files

--- crunchbase.R
+++ crunchbase.R
@@ -18,33 +18,38 @@ library(dplyr)
 #' @param fname the name of the file
 #'
 #' @return a data.frame containing the data
-read_crunchbase <- function(fdir='data_orig',
-           fname="Crunchbasecompanies-25-05-2018.csv"){
+read_1_crunchbase <- function(fdir='data_orig',
+                            fname="Crunchbasecompanies-25-05-2018.csv"){
   
   fpath <- file.path(fdir, fname)
-  df <- readr::read_delim(fpath, delim=';', col_types = 
-                                cols(
-                                  `Organization Name` = col_character(),
-                                  `Organization Name URL` = col_character(),
-                                  Categories = col_character(),
-                                  `Headquarters Location` = col_character(),
-                                  Description = col_character(),
-                                  `CB Rank (Company)` = col_integer(),
-                                  Website = col_character(),
-                                  `Founded Date` = col_date(format="%d/%m/%Y"),
-                                  `Company Type` = col_character(),
-                                  `Number of Employees` = col_character(),
-                                  `Number of Founders` = col_integer(),
-                                  `Number of Funding Rounds` = col_integer(),
-                                  `Last Funding Date` = col_date(format="%d/%m/%Y"),
-                                  `Last Funding Amount` = col_character(),
-                                  `Acquisition Status` = col_character(),
-                                  X16 = col_character()
-                                ))
+  df <- readr::read_delim(fpath, delim=',', escape_double=TRUE,col_types = 
+                            cols(
+                              `Organization Name` = col_character(),
+                              `Organization Name URL` = col_character(),
+                              Categories = col_character(),
+                              `Headquarters Location` = col_character(),
+                              Description = col_character(),
+                              `CB Rank (Company)` = col_character()
+                            ))
   df['Company'] <- df['Organization Name'] 
-  #df = df[, -c("X16")]
   return (df)
-}  
+} 
+
+read_crunchbase_files <- function(){
+  all_files <- list.files("data_orig")
+  csv_files <- all_files[grep("companies-26-06-2018.*.csv", all_files)]
+  df <- data.frame()
+  for (fi in seq_along(csv_files)){
+    print(csv_files[fi])
+    temp <- read_1_crunchbase(fdir="data_orig", fname=csv_files[fi])
+    if (nrow(df) == 0){
+      df <- temp
+    } else{
+      df <- rbind(df, temp)
+    }
+  } 
+  return (df)
+}
 
 drop_first <- function(x){
   if (length(x) > 1){
@@ -98,33 +103,32 @@ process_descriptions <- function(df){
   stop_words_regex <- paste0(" ", paste0(stop_words, collapse=" | "), " ")
   df$Desc_mod <- sapply(df$Desc_mod, str_replace_all, stop_words_regex, " ") 
 
-  df$Dwords <- strsplit(x=df$Desc_mod, split=' ')
-  df$Dwords <- sapply(df$Dwords, drop_first )
-  df$Dwords <- sapply(df$Dwords, drop_empty )
   df$Categories_lower <- sapply(df$Categories, str_to_lower)
-  df$Catwords <- strsplit(x=df$Categories_lower, split=' ')  
   #df <- select(df, Company, company_lower,  Dwords, Desc_mod,  Description, everything())
   return (df)
 }
 
-#res = df[, c('Company', 'Description', "Desc2", "Categories")]  
+df <-  read_crunchbase_files()
 
-df = read_crunchbase()
-df <-  subset(df, select= - c(X16))
-glimpse(df)
 df <- process_descriptions(df)
-glimpse(df)
 
+Dwords <- strsplit(x=df$Desc_mod, split=' ')
+Dwords <- sapply(df$Dwords, drop_first )
+Dwords <- sapply(df$Dwords, drop_empty )
 unique_dwords = unique(unlist(df$Dwords))
 cols = c("cnt")
 dwordsdf <- data.frame(matrix(nrow=length(unique_dwords), ncol=length(cols)))
 colnames(dwordsdf) <- cols
 dwordsdf$cnt <- rep(0, nrow(dwordsdf))
 row.names(dwordsdf) <- unique_dwords
+
 for (i in seq(1, nrow(df))){
   dwords <- unique(df$Dwords[i][[1]])
-  for (j in seq_along(dwords)){
-    dword <- dwords[j]
+  if (length(dwords) == 0){
+    next
+  }
+  for (j in seq_along(dwords[[1]])){
+    dword <- dwords[[1]][j]
     print (paste(j, dword))
     dwordsdf[dword, "cnt"] <- dwordsdf[dword, "cnt"] + 1
   }
@@ -133,15 +137,20 @@ for (i in seq(1, nrow(df))){
 df$Dword_wts = rep(0, nrow(df))
 for (i in seq(1, nrow(df))){
   print (i)
-  dwords <- df$Dwords[i][[1]]
-  word_wts = 1.0/dwordsdf[df$Dwords[i][[1]],] 
+  
+  dwords_list <- df$Dwords[i][[1]]
+  if (length(dwords_list) == 0){
+    next
+  }
+  dwords <- dwords_list[[1]]
+  word_wts = 1.0/dwordsdf[dwords,] 
   word_wts =  round(word_wts/sum(word_wts), 3)
   print(word_wts)
   df$Dword_wts[i] = list(word_wts)
 }
 
 #
-
+write.csv
 
 #
 
